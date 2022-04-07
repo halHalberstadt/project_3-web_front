5e55bc3f58da8fccef53af9a8726248d9e67e51f
halHalberstadt <halberstadtsterling@gmail.com>
2022-04-06 17:47:49 -0700

    Initial commit

---
+++ src/main/java/com/project_3/web_front/Project3WebFrontGroup2Application.java
@@ -0,0 +1,13 @@
+package com.project_3.web_front;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Project3WebFrontGroup2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Project3WebFrontGroup2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/project_3/web_front/Project3WebFrontGroup2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project_3.web_front;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Project3WebFrontGroup2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
